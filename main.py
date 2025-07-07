7adf619b3af306715f36a8a7963e20313a3a922b
Sartc00l <sukindima025@gmail.com>
2025-07-07 15:19:28 +0300

    Database added
    Set up sqlachemy,  added a title in site

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newflask.db'
+app.config['SQLACHEMY_TRACK-_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(300),nullable=False)
+    txt = db.Column(db.Text,nullable=False)
 
 
 @app.route("/index")
